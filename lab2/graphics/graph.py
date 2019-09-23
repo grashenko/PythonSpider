a538856921ba49e52de12732a0be8763085e5038
Alexander Grashenko <grashenko1410@mail.ru>
2019-09-23 09:31:02 +0300

    qwe

---
+++ lab2/graphics/graph.py
@@ -0,0 +1,54 @@
+# !pip install brewer2mpl
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings; warnings.filterwarnings(action='once')
+
+large = 22; med = 16; small = 12
+params = {'axes.titlesize': large,
+          'legend.fontsize': med,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': med,
+          'axes.titlesize': med,
+          'xtick.labelsize': med,
+          'ytick.labelsize': med,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+plt.style.use('seaborn-whitegrid')
+sns.set_style("white")
+
+# Version
+print(mpl.__version__)  #> 3.0.0
+print(sns.__version__)  #> 0.9.0
+
+df_raw = pd.read_json(r"F:\GitHub\PythonSpider\lab2\out.json")
+frequencyOfLength = df_raw['frequencyOfLength']
+firstSite = frequencyOfLength[0]
+df = pd.DataFrame(firstSite)
+df.sort_values('countWords', inplace=True)
+df.reset_index(inplace=True)
+
+# Draw plot
+import matplotlib.patches as patches
+
+fig, ax = plt.subplots(figsize=(16,10), facecolor='white', dpi= 80)
+ax.vlines(x=df.index, ymin=0, ymax=df.countWords, color='firebrick', alpha=0.7, linewidth=20)
+
+# Annotate Text
+for i, cty in enumerate(df.countWords):
+    ax.text(i, cty+0.5, round(cty, 1), horizontalalignment='center')
+
+
+# Title, Label, Ticks and Ylim
+ax.set_title('Count letters', fontdict={'size':22})
+ax.set(ylabel='Count words with this letters count', ylim=(0, 30))
+plt.xticks(df.index, df.countLetters, rotation=60, horizontalalignment='right', fontsize=12)
+
+# Add patches to color the X axis labels
+p1 = patches.Rectangle((.57, -0.005), width=.33, height=.13, alpha=.1, facecolor='green', transform=fig.transFigure)
+p2 = patches.Rectangle((.124, -0.005), width=.446, height=.13, alpha=.1, facecolor='red', transform=fig.transFigure)
+fig.add_artist(p1)
+fig.add_artist(p2)
+plt.show()
